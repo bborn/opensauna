1fc41deb92ba3992baf54eea848b553b7913450c
bruno bornsztein <bruno.bornsztein@gmail.com>
2014-10-23 14:08:05 -0500

    add app.json

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
-if !ENV["REDISCLOUD_URL"].blank?
-  uri = URI.parse(ENV["REDISCLOUD_URL"])
+uri_string = ENV['REDIS_URL'] || ENV['REDISTOGO_URL'] || ENV['REDISCLOUD_URL']
+if uri_string
+  uri = URI.parse(uri_string)
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
   REDIS = Redis.new
